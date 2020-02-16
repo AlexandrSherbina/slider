57b06b067094456fb42e55f89d61a903aa2829fd
AlexandrSherbina <daos147@gmail.com>
2020-02-16 15:04:19 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,99 @@
+var swiper = new Swiper('.swiper-container', {
+  speed: 600,
+  parallax: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+var swiper2 = new Swiper('.swiper-container-2', {
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination-2',
+    clickable: true,
+  },
+});
+
+var swiper3 = new Swiper('.swiper-container-3', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination-3',
+    clickable: true,
+  },
+});
+
+var swiper4 = new Swiper('.swiper-container-4', {
+  effect: 'cube',
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+  pagination: {
+    el: '.swiper-pagination-4',
+  },
+});
+
+var menuButton = document.querySelector('.menu-button');
+var openMenu = function () {
+  swiper.slidePrev();
+};
+var swiper5 = new Swiper('.swiper-container-5', {
+  slidesPerView: 'auto',
+  initialSlide: 1,
+  resistanceRatio: 0,
+  slideToClickedSlide: true,
+  on: {
+    slideChangeTransitionStart: function () {
+      var slider = this;
+      if (slider.activeIndex === 0) {
+        menuButton.classList.add('cross');
+        // required because of slideToClickedSlide
+        menuButton.removeEventListener('click', openMenu, true);
+      } else {
+        menuButton.classList.remove('cross');
+      }
+    }
+    , slideChangeTransitionEnd: function () {
+      var slider = this;
+      if (slider.activeIndex === 1) {
+        menuButton.addEventListener('click', openMenu, true);
+      }
+    },
+  }
+});
+
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+				
